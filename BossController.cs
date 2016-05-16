616e9d059e9cc843e89ec8591b729c10dc4acdca
IsaacNewton321 <isaacduartevilla@gmail.com>
2016-05-16 15:01:10 -0700

    Update BossController.cs

--- BossController.cs
+++ BossController.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 
 public class BossController : MonoBehaviour {
-
+public float time = 100;
+public float startingHealth = 10;
+public float currentHealth;
 	// Use this for initialization
 	void Start () {
-	
+	currentHealth = startingHealth;
 	}
 	
 	// Update is called once per frame
