fed817b1081227899c848175ef394f77588a4443
IsaacNewton321 <isaacduartevilla@gmail.com>
2016-05-17 16:30:37 -0700

    Add timer and projectile reactions
    set up the frames so that something can happen when the timer hits a certain point, and when the boss is hit by two different kinds of projectiles.

--- BossController.cs
+++ BossController.cs
@@ -5,6 +5,10 @@ public class BossController : MonoBehaviour {
 public float time = 100;
 public float startingHealth = 10;
 public float currentHealth;
+public float bulletDamage = 1;
+public float boostedBulletDamage = 2;
+int isNinety = 0;
+int isSeventyFive = 0;
 	// Use this for initialization
 	void Start () {
 	currentHealth = startingHealth;
@@ -12,6 +16,27 @@ public float currentHealth;
 	
 	// Update is called once per frame
 	void Update () {
+	time -= Time.deltaTime;
+	if(Mathf.Round(time) == 90 && isNinety == 0)
+	{
+		isNinety = 1;
+	}
+	if(Mathf.Round(time) == 75 && isSeventyFive == 0)
+	{
+		isSeventyFive == 1;
+	}
+	}
 	
+	void OnTriggerEnter(Collider collision)
+	{
+		switch(collision.gameObject.tag)
+		{
+			case "Projectile":
+			currentHealth -= bulletDamage;
+			break;
+			case "BoostProjectile":
+			currentHealth -= boostedBulletDamage;
+			break;
+		}
 	}
 }
