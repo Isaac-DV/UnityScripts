ead830c525b2aea890ed39a90b2e7dd684067ef5
IsaacNewton321 <isaacduartevilla@gmail.com>
2016-05-19 12:12:15 -0700

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -23,11 +23,33 @@ public class PlayerController : MonoBehaviour {
 	int isGunBoost = 0;
 	float gunBoostEnd;
 	int isPlatform;
-	
+
 	public GameObject bulletPrefab;
 	public GameObject boostedBulletPrefab;
 	public GameObject shotSpawn;
 	public float bulletImpulse;
+
+	public GameObject level1Start;
+	public GameObject level2Start;
+	public GameObject level3Start;
+	public GameObject level4Start;
+	public GameObject level5Start;
+	public GameObject level6Start;
+	public GameObject level1End;
+	public GameObject level2End;
+	public GameObject level3End;
+	public GameObject level4End;
+	public GameObject level5End;
+	public GameObject level6End;
+	public GameObject level1Hub;
+	public GameObject level2Hub;
+	public GameObject level3Hub;
+	public GameObject level4Hub;
+	public GameObject level5Hub;
+	public GameObject level6Hub;
+	public GameObject hub;
+
+	int inHub = ;
 
 	CharacterController characterController;
 	// Use this for initialization
@@ -37,7 +59,7 @@ public class PlayerController : MonoBehaviour {
 		counter.text = "Points: 0";
 		healthDisplay.text = "Health: " + startingHealth.ToString ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		float sideSpeed = Input.GetAxis ("Horizontal") * movementSpeed;
@@ -64,7 +86,7 @@ public class PlayerController : MonoBehaviour {
 		}
 		if(time == shieldBreak && isShield == 1)
 		{
-			isShield == 0;
+			isShield = 0;
 		}
 		if (Input.GetButtonDown ("Fire1"))
 		{
@@ -89,24 +111,9 @@ public class PlayerController : MonoBehaviour {
 
 		}
 	}
-		
+
 	void OnTriggerEnter(Collider other) 
 	{
-		// Determine if a collision between Player and Pick Up object has occured
-		/*if (other.gameObject.CompareTag("Pickup"))
-		{
-			other.gameObject.SetActive(false);
-			pickupCount += 1;
-			counter.text = "Points: " + pickupCount.ToString ();
-		}
-		if (other.gameObject.CompareTag ("Enemy")) 
-		{
-			currentHealth -= 10;
-		}
-		if (other.gameObject.CompareTag ("Pit")) 
-		{
-			currentHealth = 0.0f;
-		}*/
 		switch (other.gameObject.tag) {
 		case "Pickup":
 			other.gameObject.SetActive(false);
@@ -135,14 +142,75 @@ public class PlayerController : MonoBehaviour {
 			isGunBoost = 1;
 			gunBoostEnd = time - 10;
 			break;
-		
+
 		case "Platform":
 			isPlatform = 1;
 			break;
-			
+
 		case "BossTrigger":
-			Boss.gameObject.SetActive(True);
+			Boss.gameObject.SetActive(true);
 			break;
+		}
+
+		if (other.gameObject == level1Hub) 
+		{
+			transform.position = level1Start.transform.position;
+			time = 100;
+		}
+		if (other.gameObject == level2Hub) 
+		{
+			transform.position = level2Start.transform.position;
+			time = 100;
+		}
+		if (other.gameObject == level3Hub) 
+		{
+			transform.position = level3Start.transform.position;
+			time = 100;
+		}
+		if (other.gameObject == level4Hub) 
+		{
+			transform.position = level4Start.transform.position;
+			time = 100;
+		}
+		if (other.gameObject == level5Hub) 
+		{
+			transform.position = level5Start.transform.position;
+			time = 100;
+		}
+		if (other.gameObject == level6Hub) 
+		{
+			transform.position = level6Start.transform.position;
+			time = 100;
+		}
+		if (other.gameObject == level1End) 
+		{
+			transform.position = hub.transform.position;
+			time = 5000;
+		}
+		if (other.gameObject == level2End) 
+		{
+			transform.position = hub.transform.position;
+			time = 5000;
+		}
+		if (other.gameObject == level3End) 
+		{
+			transform.position = hub.transform.position;
+			time = 5000;
+		}
+		if (other.gameObject == level4End) 
+		{
+			transform.position = hub.transform.position;
+			time = 5000;
+		}
+		if (other.gameObject == level5End) 
+		{
+			transform.position = hub.transform.position;
+			time = 5000;
+		}
+		if (other.gameObject == level6End) 
+		{
+			transform.position = hub.transform.position;
+			time = 5000;
 		}
 	}
 }
