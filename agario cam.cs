054518c31409590d9ea4b4898bff3b70ffd5a770
IsaacNewton321 <isaacduartevilla@gmail.com>
2016-05-27 14:21:52 -0700

    Update agario cam.cs

--- agario cam.cs
+++ agario cam.cs
@@ -1,1 +1,25 @@
-
+using UnityEngine;
+using System.Collections;
+ 
+public class Eat : MonoBehaviour
+{
+    public string Tag;
+    public float Increase;
+    public Camera playerCam;
+    float size;
+    public float distance = 10;
+ 
+    void Update(){
+      size = transform.localScale.y;
+      playerCam.transform.position.y = size + distance;
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == Tag)
+        {
+            transform.localScale += new Vector3(Increase, Increase, Increase);
+            Destroy(other.gameObject);
+        }
+    }
+}
