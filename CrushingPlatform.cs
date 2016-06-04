e48135214257d41e013baea9831a8456693bf1f2
IsaacNewton321 <isaacduartevilla@gmail.com>
2016-06-03 22:57:14 -0700

    Update CrushingPlatform.cs

--- CrushingPlatform.cs
+++ CrushingPlatform.cs
@@ -2,32 +2,32 @@ using UnityEngine;
 using System.Collections;
 
 public class CrushingPlatform : MonoBehaviour {
-  //Put this script on the object(plane) that the player
-  //steps on to activate the crushing platform, not on the crushing platform itself.
-  public float speed;
-  public GameObject crushingPlatform;
-  bool isFalling = 0;
-  
-  void Start(){
-    
-  }
-  void Update(){
-    if(isFalling == 1)
-    {
-      crushingPlatform.transform.Translate(0, -time.DeltaTime*speed, 0);
-    }
-  }
-  void OnTriggerEnter(Collider other)
-  {
-    switch(other.gameObject.tag)
-    {
-      case "Player":
-      isFalling = 1;
-      break;
-      case "Crush":
-      isFalling = 0;
-      break;
-    }
-  }
-  
+	//Put this script on the object(plane) that the player
+	//steps on to activate the crushing platform, not on the crushing platform itself.
+	public float speed;
+	public GameObject crushingPlatform;
+	int isFalling = 0;
+
+	void Start(){
+		
+	}
+	void Update(){
+		if(isFalling == 1)
+		{
+			crushingPlatform.transform.Translate(0, -Time.deltaTime*speed, 0);
+		}
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		switch(other.gameObject.tag)
+		{
+		case "Player":
+			isFalling = 1;
+			break;
+		case "Crush":
+			isFalling = 0;
+			break;
+		}
+	}
+
 }
