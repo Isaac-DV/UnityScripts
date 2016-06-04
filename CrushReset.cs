da3a51991613376f1725656113929b86b4a97a25
IsaacNewton321 <isaacduartevilla@gmail.com>
2016-06-03 22:58:26 -0700

    Update CrushReset.cs

--- CrushReset.cs
+++ CrushReset.cs
@@ -1,1 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class CrushReset : MonoBehaviour {
+	//Put this scrpit on the actual platform that is falling
+	private Vector3 tr;
+	// Use this for initialization
+	void Start () {
+		tr = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ResetPosition(){
+		transform.position = tr;
+	}
+}
 
