0649ecd22bfcf7a60003a5429aac949e56363f40
IsaacNewton321 <isaacduartevilla@gmail.com>
2016-06-06 14:50:34 -0700

    Update CameraController.cs

--- CameraController.cs
+++ CameraController.cs
@@ -1,1 +1,32 @@
+using UnityEngine;
+using System.Collections;
 
+public class CameraController : MonoBehaviour {
+  public float camPos1;
+  public float camPos2;
+  bool isPos1 = true;
+  bool isPos2 = false;
+  
+  void Start(){
+    transform.position = new Vector3(transform.position.x, camPos1, transform.position.z);
+  }
+  void Update(){
+    if(Input.GetKeyDown("w") || Input.GetButtonDown("s"))
+    {
+      if(isPos1 == true)
+      {
+        transform.position = new Vector3(transform.position.x, camPos2, transform.position.z);
+        isPos1 = false;
+        isPos2 = true;
+      }
+      else if(isPos2 = true)
+      {
+        transform.position = new Vector3(transform.position.x, camPos1, transform.position.z);
+        isPos2 = false;
+        isPos1 = true;
+      }
+    }
+  }
+  
+  
+}
