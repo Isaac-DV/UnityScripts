e6b8c5b5d0d2db21c185e3c85c517bce12fef6c4
IsaacNewton321 <isaacduartevilla@gmail.com>
2016-06-06 20:01:28 -0700

    Update Underling.cs

--- Underling.cs
+++ Underling.cs
@@ -1,1 +1,56 @@
+using UnityEngine;
+using System.Collections;
 
+public class Underling : MonoBehaviour {
+  float min = 2f;
+	float max = 3f;
+  float distance = 25;
+	public float speed = 10;
+	public float currentHealth;
+	public float startingHealth = 2;
+	public float bulletDamage = 1;
+	public float boostedBulletDamage = 2;
+	private BossController script;
+	public GameObject Boss;
+	// Use this for initialization
+	void Start () {
+		min = transform.position.x;
+		max = transform.position.x + distance;
+		currentHealth = startingHealth;
+		script = Boss.GetComponent<BossController>();
+		script.enemyCount ++;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (Mathf.PingPong (Time.time * speed, max - min) + min, transform.position.y, transform.position.z);
+		time -= Time.deltaTime;
+		if (time <= 0) 
+		{
+			currentHealth = 0;
+		}
+		if(currentHealth <= 0)
+		{
+			destroyEnemy();
+		}
+	}
+	
+	void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.tag == "Projectile")
+		{
+			currenthealth -= bulletDamage;
+			Destroy(collision.gameObject);
+		}
+		if(collision.gameObject.tag == "BoostProjectile")
+		{
+			currentHealth -= boostedBulletDamage;
+			Destroy(collision.gameObject);
+		}
+	}
+	void destroyEnemy()
+	{
+		Destroy(gameObject);
+		script.enemyCount -= 1;
+	}
+}
