b09eeda9177f1dcc4dfe7873c04f57d817b15cf9
IsaacNewton321 <isaacduartevilla@gmail.com>
2016-06-06 21:45:23 -0700

    Update BossController.cs

--- BossController.cs
+++ BossController.cs
@@ -9,6 +9,11 @@ public class BossController : MonoBehaviour {
 	public float boostedBulletDamage = 2;
 	public GameObject win;
 	public GameObject underlingPrefab;
+	public GameObject enemySpawn1;
+	public GameObject enemySpawn2;
+	public GameObject enemySpawn3;
+	public GameObject enemySpawn4;
+	public GameObject enemySpawn5;
 
 	public GameObject bulletPrefab;
 	public GameObject shotSpawn;
@@ -20,7 +25,7 @@ public class BossController : MonoBehaviour {
 	int isSeventyFive = 0;
 	int thirdSpawn = 0;
 	int canHit = 1;
-	public int enemyCount;
+	public int enemyCount = 0;
 	// Use this for initialization
 	void Start () {
 		currentHealth = startingHealth;
@@ -33,26 +38,29 @@ public class BossController : MonoBehaviour {
 		if(Mathf.Round(time) == 177 && isOneSeventySeven == 0)
 		{
 			isOneSeventySeven = 1;
-			for(int i = 0;i<10;i++)
-			{
-			GameObject enemy = (GameObject)Instantiate(underlingPrefab, shotSpawn.transform.position, shotSpawn.transform.rotation);
-			}
+			GameObject enemy = (GameObject)Instantiate(underlingPrefab, enemySpawn1.transform.position, enemySpawn1.transform.rotation);
+			GameObject enemy2 = (GameObject)Instantiate(underlingPrefab, enemySpawn2.transform.position, enemySpawn2.transform.rotation);
+			GameObject enemy3 = (GameObject)Instantiate(underlingPrefab, enemySpawn3.transform.position, enemySpawn3.transform.rotation);
+			GameObject enemy4 = (GameObject)Instantiate(underlingPrefab, enemySpawn4.transform.position, enemySpawn4.transform.rotation);
+			GameObject enemy5 = (GameObject)Instantiate(underlingPrefab, enemySpawn5.transform.position, enemySpawn5.transform.rotation);
 		}
 		if(Mathf.Round(time) == 120 && isSeventyFive == 0)
 		{
 			isSeventyFive = 1;
-			for(int i = 0;i<10;i++)
-			{
-			GameObject enemy = (GameObject)Instantiate(underlingPrefab, shotSpawn.transform.position, shotSpawn.transform.rotation);
-			}
+			GameObject enemy = (GameObject)Instantiate(underlingPrefab, enemySpawn1.transform.position, enemySpawn1.transform.rotation);
+			GameObject enemy2 = (GameObject)Instantiate(underlingPrefab, enemySpawn2.transform.position, enemySpawn2.transform.rotation);
+			GameObject enemy3 = (GameObject)Instantiate(underlingPrefab, enemySpawn3.transform.position, enemySpawn3.transform.rotation);
+			GameObject enemy4 = (GameObject)Instantiate(underlingPrefab, enemySpawn4.transform.position, enemySpawn4.transform.rotation);
+			GameObject enemy5 = (GameObject)Instantiate(underlingPrefab, enemySpawn5.transform.position, enemySpawn5.transform.rotation);
 		}
 		if(Mathf.Round(time) == 60 && thirdSpawn == 0)
 		{
 			isSeventyFive = 1;
-			for(int i = 0;i<10;i++)
-			{
-			GameObject enemy = (GameObject)Instantiate(underlingPrefab, shotSpawn.transform.position, shotSpawn.transform.rotation);
-			}
+			GameObject enemy = (GameObject)Instantiate(underlingPrefab, enemySpawn1.transform.position, enemySpawn1.transform.rotation);
+			GameObject enemy2 = (GameObject)Instantiate(underlingPrefab, enemySpawn2.transform.position, enemySpawn2.transform.rotation);
+			GameObject enemy3 = (GameObject)Instantiate(underlingPrefab, enemySpawn3.transform.position, enemySpawn3.transform.rotation);
+			GameObject enemy4 = (GameObject)Instantiate(underlingPrefab, enemySpawn4.transform.position, enemySpawn4.transform.rotation);
+			GameObject enemy5 = (GameObject)Instantiate(underlingPrefab, enemySpawn5.transform.position, enemySpawn5.transform.rotation);
 		}
 		if(enemyCount <= 0)
 		{
