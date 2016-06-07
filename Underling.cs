dbc26b5a2307a1f42cd7fc102be403695ba9e0ba
IsaacNewton321 <isaacduartevilla@gmail.com>
2016-06-06 21:24:43 -0700

    Update Underling.cs

--- Underling.cs
+++ Underling.cs
@@ -5,20 +5,21 @@ public class Underling : MonoBehaviour {
   float min = 2f;
 	float max = 3f;
   float distance = 25;
+  public float time;
 	public float speed = 10;
 	public float currentHealth;
 	public float startingHealth = 2;
 	public float bulletDamage = 1;
 	public float boostedBulletDamage = 2;
 	private BossController script;
-	public GameObject Boss;
 	// Use this for initialization
 	void Start () {
 		min = transform.position.x;
 		max = transform.position.x + distance;
 		currentHealth = startingHealth;
-		script = Boss.GetComponent<BossController>();
+		script = GameObject.Find("BossDragon").GetComponent<BossController>();
 		script.enemyCount ++;
+		time = script.time;
 	}
 	
 	// Update is called once per frame
