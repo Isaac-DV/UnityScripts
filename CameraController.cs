482e7c6bcf8ffc4978283dcd79ec77747c8cadc6
IsaacNewton321 <isaacduartevilla@gmail.com>
2016-06-06 21:58:17 -0700

    Update CameraController.cs

--- CameraController.cs
+++ CameraController.cs
@@ -2,26 +2,28 @@ using UnityEngine;
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
-	public float camPos1;
-	public float camPos2;
+	Vector3 camPos1;
+	Vector3 camPos2;
 	bool isPos1 = true;
 	bool isPos2 = false;
+	public float distanceBetweenPos;
 
 	void Start(){
-		transform.position = new Vector3(transform.position.x, transform.position.y, camPos1);
+		camPos1 = transform.position;
+		camPos2 = (transform.position.x, transform.position.y, transform.position.z - distanceBetweenPos);
 	}
 	void Update(){
 		if(Input.GetKeyDown("w") || Input.GetKeyDown("s"))
 		{
 			if(isPos1 == true)
 			{
-				transform.position = new Vector3(transform.position.x, transform.position.y, camPos2);
+				transform.position = camPos2;
 				isPos1 = false;
 				isPos2 = true;
 			}
 			else if(isPos2 == true)
 			{
-				transform.position = new Vector3(transform.position.x, transform.position.y, camPos1);
+				transform.position = camPos1;
 				isPos2 = false;
 				isPos1 = true;
 			}
